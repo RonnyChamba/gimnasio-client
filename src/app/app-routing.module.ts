84277bf7282de999ac0b36e8e68d281754f0cb95
ronny <ronnychamba96@gmail.com>
2023-02-11 17:38:52 -0500

    disegn login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormLoginComponent } from './auth/form-login/form-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full',redirectTo:'login' },
+  { path: 'login', component: FormLoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
