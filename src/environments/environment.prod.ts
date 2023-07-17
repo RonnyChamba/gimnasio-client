727562f2217fcd59d6b8dc301c631f23e8e4183e
ronnychamba <ronnychamba96@gmail.com>
2023-07-16 21:17:33 -0500

    add config execute

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment= {
 
     production: true,
-    _APIUrl: "http://localhost:8080/api/v1"
+    // _APIUrl: "http://localhost:8080/api/v1"
+    _APIUrl: "http://146.190.126.117:8081/api/v1"
 
 }
